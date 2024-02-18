9d22f7e84a754f6f5d22d997a5183ef75c307d91
Monique Caldeira <nique.sca@gmail.com>
2024-02-17 21:34:04 -0300

    criação do componente Product

---
+++ src/components/product.tsx
@@ -0,0 +1,33 @@
+import { TouchableOpacity, TouchableOpacityProps, Image, ImageProps, View, Text } from "react-native";
+
+type ProductDataProps = {
+   title: string
+   description: string
+   thumbnail: ImageProps
+}
+
+type ProductProps = TouchableOpacityProps & {
+   data: ProductDataProps
+}
+
+
+export function Product({ data, ...rest } : ProductProps) {
+   return(
+      <TouchableOpacity 
+         className="w-full flex-row items-center pb-4"
+         {...rest}   
+      >
+         <Image source={data.thumbnail} className="w-20 h-20 rounded-md "/>
+
+         <View className="flex-1 ml-3">
+            <Text className="text-slate-100 font-subtitle text-base flex-1">
+               {data.title}
+            </Text>
+            <Text className="text-slate-400 text-xs leading-5 mt-0.5">
+               {data.description}
+            </Text>
+         </View>
+      
+      </TouchableOpacity>
+   )
+}
